8931672c6182be17398cf8c0cbd27b1ed3c3753f
jerinmt <jerinmt379@gmail.com>
2025-04-25 16:41:06 +0530

    Added various ways of form validation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+
 const email = document.querySelector("#emailId");
 const emailError = document.querySelector("#emailId + span.error");
 
@@ -36,3 +37,86 @@ email.addEventListener("input", () => {
     // Add the `active` class
     emailError.className = "error active";
   }
+
+  //name checking
+
+  const firstName = document.querySelector("#firstName");
+  const fnameError = document.querySelector("#firstName + span.error");
+  const lastName = document.querySelector("#lastName");
+  const lnameError = document.querySelector("#lastName + span.error");
+  
+  firstName.addEventListener("input", () => {
+      if (firstName.validity.valid) {
+        fnameError.textContent = ""; // Remove the message content
+        fnameError.className = "error"; // Removes the `active` class
+      } else {
+        // If there is still an error, show the correct error
+        fnameShowError();
+      }
+    });
+    
+    form.addEventListener("submit", (event) => {
+      // if the first name field is invalid
+      if (!firstName.validity.valid) {
+        // display an appropriate error message
+        fnameShowError();
+        // prevent form submission
+        event.preventDefault();
+      }
+    });
+    
+    function fnameShowError() {
+      if (firstName.validity.valueMissing) {
+        // If empty
+        fnameError.textContent = "You need to enter a first name.";
+      } else if (firstName.validity.tooShort) {
+        // If the value is too short,
+        fnameError.textContent = `First name should be at least ${firstName.minLength} characters; you entered ${firstName.value.length}.`;
+      }
+      // Add the `active` class
+      fnameError.className = "error active";
+    }
+    
+    lastName.addEventListener("input", () => {
+      if (lastName.validity.valid) {
+        lnameError.textContent = ""; // Remove the message content
+        lnameError.className = "error"; // Removes the `active` class
+      } else {
+        // If there is still an error, show the correct error
+        lnameShowError();
+      }
+    });
+    
+    form.addEventListener("submit", (event) => {
+      // if the last name field is invalid
+      if (!lastName.validity.valid) {
+        // display an appropriate error message
+        lnameShowError();
+        // prevent form submission
+        event.preventDefault();
+      }
+    });
+    
+    function lnameShowError() {
+      if (lastName.validity.valueMissing) {
+        // If empty
+        lnameError.textContent = "You need to enter a last name.";
+      } else if (lastName.validity.tooShort) {
+        // If the value is too short,
+        lnameError.textContent = `Last name should be at least ${lastName.minLength} characters; you entered ${lastName.value.length}.`;
+      }
+      // Add the `active` class
+      lnameError.className = "error active";
+    }
+
+    const submit = document.querySelector('button');
+submit.addEventListener('click', ()=> {
+  let password1 = document.querySelector('#password').value;
+  let password2 = document.querySelector('#confirmPassword').value;
+  if(password1!==password2) {
+    alert("Password mismatch");
+  }
+  else if(form.checkValidity()) {
+    alert('Submitted successfully');
+  }
+});
